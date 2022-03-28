57021ddbcf94e20b872c5c2a5f3ca27c36c5038f
khoirinkp <khoirinkp@gmail.com>
2022-03-28 11:27:13 +0700

    first commit

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+function objek(){
+    var obj = {
+        nama: "Rizki",
+        umur: 20,
+        pekerjaan: "Mahasiswa"
+    };
+
+    console.log(obj);
+
+    console.log("");
+    obj.pekerjaan = "Pegawai";
+    console.log(obj);
+}
+objek();
